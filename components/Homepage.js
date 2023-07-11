d28f7990511024ecec1ff8c68bac1f4f9e77c7a2
Towhid <towhidgolam8182@gmail.com>
2023-07-11 23:11:35 +0600

    homepage and basic states initialized

---
+++ components/Homepage.js
@@ -0,0 +1,74 @@
+import { Box, Button, Input, TextField, Typography } from '@mui/material';
+import { useState } from 'react';
+
+export default function Homepage() {
+  const [longUrl, setLongUrl] = useState('');
+  const [shortUrl, setShortUrl] = useState('');
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setLongUrl(e.target.value);
+  };
+
+  const handleClick = () => {
+    const shortLink = 'sqz.ly/' + longUrl;
+    setShortUrl(shortLink);
+    setLongUrl('');
+  };
+
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        height: '100vh',
+      }}
+    >
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          gap: '10px',
+
+          minWidth: '75%',
+        }}
+      >
+        <TextField
+          variant="outlined"
+          placeholder="Enter complete URL to shorten..."
+          sx={{ minWidth: '75%' }}
+          onChange={(e) => handleChange(e)}
+          value={longUrl}
+        />
+
+        <Button
+          variant="outlined"
+          sx={{ padding: '15px' }}
+          onClick={handleClick}
+        >
+          Generate
+        </Button>
+      </Box>
+
+      <Box sx={{}}>
+        <Typography variant="h5">Here's the shortened link: </Typography>
+        {/* <Typography variant="h6">{shortUrl}</Typography> */}
+        <TextField
+          variant="outlined"
+          id="outlined-read-only-input"
+          value={shortUrl}
+          InputProps={{
+            readOnly: true,
+          }}
+        />
+        <Box>
+          <Button variant="outlined">Copy</Button>
+          <Button variant="outlined">Go to</Button>
+        </Box>
+      </Box>
+    </Box>
+  );
+}
