cdddf9ee6f759793b3bd35acb4677bc0e999f2ec
Towhid <towhidgolam8182@gmail.com>
2023-07-20 21:53:27 +0600

    working on netlify rounting issue

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
-  trailingSlash: true,
-  distDir: 'dist',
 };
 
 module.exports = nextConfig;
